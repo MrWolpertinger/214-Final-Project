3cc000f12c60b050d89ebed803729cf2e466d8d5
Daniel Bezuidenhout <u21539686@tuks.co.za>
2022-11-03 11:04:12 +0200

    Add files via upload
    Added changes to weapon factory to check the production matches budget, as well as specifying the amount of weapons to be made

---
+++ Task-3/WeaponsFactory/HeavyWeightFactory.h
@@ -0,0 +1,14 @@
+#ifndef HEAVYWEIGHTFACTORY_H
+#define HEAVYWEIGHTFACTORY_H
+#include "WeaponsFactory.h"
+#include "../Weapons.h"
+
+class HeavyWeightFactory : public WeaponsFactory
+{
+    public:
+        Weapons* produceWeapons(int choice, int num, int budget);
+        HeavyWeightFactory(){};
+        ~HeavyWeightFactory(){};
+};
+
+#endif
