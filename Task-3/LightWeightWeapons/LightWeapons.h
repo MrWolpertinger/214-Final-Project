380742f9252f3118920710cc4d336899d5a12650
Daniel Bezuidenhout <u21539686@tuks.co.za>
2022-11-03 11:05:06 +0200

    Add files via upload
    Added additional variables to specify amount of weapons, as well as default prices

---
+++ Task-3/LightWeightWeapons/LightWeapons.h
@@ -0,0 +1,13 @@
+#ifndef LIGHTWEAPONS_H
+#define LIGHTWEAPONS_H
+#include "../Weapons.h"
+
+/// @brief 
+class LightWeapons : public Weapons
+{
+    public: 
+        LightWeapons(int, int, string);
+        ~LightWeapons(){};
+};
+
+#endif
