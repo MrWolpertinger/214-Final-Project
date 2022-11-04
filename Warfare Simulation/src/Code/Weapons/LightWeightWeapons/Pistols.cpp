8ac96fa2e563b4ab1c9353d56e9a4e30fcd5cc33
Daniel Bezuidenhout <u21539686@tuks.co.za>
2022-11-04 10:35:00 +0200

    Update Pistols.cpp
    Fixed semantic error and added price

--- Warfare Simulation/src/Code/Weapons/LightWeightWeapons/Pistols.cpp
+++ Warfare Simulation/src/Code/Weapons/LightWeightWeapons/Pistols.cpp
@@ -1,7 +1,7 @@
 #include "Pistols.h"
 
 /// @brief Pistols Constructor also sets the predetermined damage of the Pistols Weapons
-Pistols::Pistols(int dam, int num, string name):LightWeapons(dam, num, string)
+Pistols::Pistols(int dam, int num, string name):LightWeapons(dam, num, name)
 {
-    
+    this->price = 300;
 }
