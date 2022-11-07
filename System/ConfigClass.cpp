5b51021bf952b53e6f1aa06862bcd3de013b2a9e
Derek Kropff <u19015501@tuks.co.za>
2022-11-07 21:31:38 +0200

    finished working on configClass

--- System/ConfigClass.cpp
+++ System/ConfigClass.cpp
@@ -2,6 +2,11 @@
 #include "toml.hpp"
 #include <iostream>
 
+
+ConfigClass::~ConfigClass() {
+    delete [] this->ListOfCountries;
+};
+
 ConfigClass* ConfigClass::onlyInstance_;
 
 ConfigClass* ConfigClass::instance() {
@@ -9,6 +14,7 @@ ConfigClass* ConfigClass::instance() {
         ConfigClass::onlyInstance_ = new ConfigClass();
     }
     return ConfigClass::onlyInstance_;
+    srand(time(NULL));
 };
 
 void ConfigClass::ParseConfigFile(std::string path) {
@@ -24,46 +30,60 @@ void ConfigClass::ParseConfigFile(std::string path) {
     }
     
     //make troops structs first in an array to be copied into the individual country objects
+    try {
     int TotalTroops = *tbl.at_path("Totaltroops").value<int>();
-    Force f[TotalTroops];
     
-    for(int i = 0; i < TotalTroops; i++) {
-        f[i].DMGX = *tbl.at_path(("Force" + std::to_string(i+1) + ".DMGX")).value<int>();
-        f[i].HPX = *tbl.at_path(("Force" + std::to_string(i+1) + ".HPX")).value<int>();
-        f[i].MaxTroops = *tbl.at_path(("Force" + std::to_string(i+1) + ".maxTroops")).value<int>();
-        f[i].Troops = *tbl.at_path(("Force" + std::to_string(i+1) + ".initTroops")).value<int>();
-        f[i].Country = *tbl.at_path(("Force" + std::to_string(i+1) + ".country")).value<std::string>();
-        f[i].Type = *tbl.at_path(("Force" + std::to_string(i+1) + ".type")).value<std::string>();
-    }
-
     //create and populate country
     this->SizeOfArr = *tbl.at_path("CountryNumber").value<int>();
     this->ListOfCountries = new CountryStruct[this->SizeOfArr];// allocating memory for country structs
 
     for (int i = 0; i < this->SizeOfArr; i++) {
-        this->ListOfCountries[i].NumOfForces = *tbl.at_path("Country" + std::to_string(i+1) + ".NumberOfForces").value<int>();
-        int troopAlocated = 0;
-        this->ListOfCountries[i].CountryForces = new Force[this->ListOfCountries[i].NumOfForces];
-        for(int j = 0; (j < TotalTroops && troopAlocated < this->ListOfCountries[i].NumOfForces); j++) {
-            if(f[j].Country == ("Country" + std::to_string(i+1))) {
-                this->ListOfCountries[i].CountryForces[troopAlocated++] = f[i];
-            }
-        }
-        //added Force structs to country structs
-        std::string side = *tbl.at_path("Country" + std::to_string(i+1) + ".Side").value<std::string>();
-        if(side == "A") {
-            this->ListOfCountries[i].side = 'A';
-        } else if(side == "B") {
-            this->ListOfCountries[i].side = 'B';
-        } else {
-            this->ListOfCountries[i].side = 'N';
+
+        this->ListOfCountries[i].NumOfForces = *tbl.at_path("Country" + std::to_string(i+1) + ".NumberOfForces").value<int>();//get size of array
+        //this->ListOfCountries[i].CountryForces = new Force[this->ListOfCountries[i].NumOfForces];// allocate array
+
+        for(int j = 0; j < this->ListOfCountries[i].NumOfForces; j++) {//add a random troop to country
+            int x = (rand() % TotalTroops);
+            this->ListOfCountries[i].CountryForces[j].DMGX = *tbl.at_path(("Force" + std::to_string(x) + ".DMGX")).value<int>();
+            this->ListOfCountries[i].CountryForces[j].HPX = *tbl.at_path(("Force" + std::to_string(x) + ".HPX")).value<int>();
+            this->ListOfCountries[i].CountryForces[j].Troops = *tbl.at_path(("Force" + std::to_string(x) + ".initTroops")).value<int>();
+            this->ListOfCountries[i].CountryForces[j].Type = *tbl.at_path(("Force" + std::to_string(x) + ".type")).value<std::string>();
         }
 
         this->ListOfCountries[i].supportX = *tbl.at_path("Country" + std::to_string(i+1) + ".supportMultiplier").value<int>();
+        this->ListOfCountries[i].TotalSpending = *tbl.at_path("Country" + std::to_string(i+1) + ".totalSpending").value<double>();
+
+        this->ListOfCountries[i].name = *tbl.at_path("Country" + std::to_string(i+1) + ".Name").value<std::string>();
+        this->ListOfCountries[i].leaderName = *tbl.at_path("Country" + std::to_string(i+1) + ".Leader").value<std::string>();
 
         for(int j = 0; j < 8; j++) {
             this->ListOfCountries[i].NoOfWeapons[j] =  *tbl.at_path("Country" + std::to_string(i+1) + ".NumberOfWeapons")[j].value<int>();
             this->ListOfCountries[i].WeaponDMGX[j] =  *tbl.at_path("Country" + std::to_string(i+1) + ".WeaponsMultiplier")[j].value<int>();
+            this->ListOfCountries[i].CountryStats[j] = *tbl.at_path("Country" + std::to_string(i+1) + ".CountryStats")[j].value<int>();
         }
     }
+    } catch (const std::bad_alloc& e) {
+        std::cout << e.what() << std::endl;
+    }
+};
+
+CountryStruct* ConfigClass::getUniqueCountry() {
+    bool found = false;
+    for(int i = 0; i < this->SizeOfArr; i++) {
+        if(this->ListOfCountries[i].HasBeenUsed == false) {
+            found = true;
+        }
+    }
+    if (found == false) {
+        return nullptr;
+    }
+    
+    while(found == true) {
+        int x = rand() % this->SizeOfArr;
+        if(this->ListOfCountries[x].HasBeenUsed == false) {
+            this->ListOfCountries[x].HasBeenUsed = true;
+            return &this->ListOfCountries[x];
+        }
+    }
+    return nullptr;
 };
--- System/ConfigClass.h
+++ System/ConfigClass.h
@@ -5,20 +5,21 @@
 struct Force {
     int DMGX;
     int HPX;
-    int MaxTroops;
     int Troops;// current size of dynamic arr
-    std::string Country;
     std::string Type;
 };
 
 struct CountryStruct {
     std::string name;
-    char side; //A||B||N
+    std::string leaderName;
     int supportX;
     int NoOfWeapons[8];//0-grenades 1-MachineGuns 2-Rifels 3-pistols 4-Warships 5-Submarines 6-Tanks 7-Aircrafts
     int WeaponDMGX[8];
+    int CountryStats[8];//0-refugeeCount 1-enlistedCitizens 2-deployedCitizens 3-fightingCitizens
+    double TotalSpending;//4-stationaryCitizens 5-returnedCitizens 6-deathtoll 7-unlistedCitizens
     int NumOfForces;// size of countriesForces[]+1
-    Force* CountryForces;
+    Force CountryForces[25];//max number of forces cannot exceed 25
+    bool HasBeenUsed = false;
 };
 
 class ConfigClass
@@ -29,10 +30,12 @@ public:
     int SizeOfArr;// size of ListOfCountries[]+1
     void ParseConfigFile(std::string path);
     static ConfigClass* instance();
+    CountryStruct* getUniqueCountry();//returns nullptr if countryarr has been exhausted
 protected:
     ConfigClass() {};
+    ~ConfigClass();
 private: 
     static ConfigClass* onlyInstance_;
 };
 
-#endif//ConfigClass::ChangableX[0] = 
+#endif
